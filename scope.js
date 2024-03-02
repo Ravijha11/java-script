c76f3ea6b696fd774c2a86a3b6703e1c01c356de
RAVINDRA JHA <ravijha110903@gmail.com>
2024-03-02 16:44:38 +0530

    arrow

--- scope.js
+++ scope.js
@@ -1,11 +1,11 @@
-// let a = 554;
-// var b = 45464;
-// const c = 445;
-// if(1==1){
-//     let a = 25;
-//     console.log(`inner loop is ${a}`)
-// }
-// console.log(`outside value is ${a}`)
+let a = 554;
+var b = 45464;
+const c = 445;
+if(1==1){
+    let a = 25;
+    console.log(`inner loop is ${a}`)
+}
+console.log(`outside value is ${a}`)
 
 
 
