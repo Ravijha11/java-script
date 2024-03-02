ea6ec7f8513d5ac94f667f176dac86ffec193099
RAVINDRA JHA <ravijha110903@gmail.com>
2024-03-02 16:43:33 +0530

    aroow

---
+++ arrow.js
@@ -0,0 +1,17 @@
+const ravi = {
+    name : "ravijha",
+    class: "Btech",
+    CGPA : "7.56",
+    collage : "Madhav institute of technology and science",
+    welcome : function(){
+        console.log(`${this.name}, welcome to our company men`)
+    }
+}
+// // console.log(ravi.welcome())
+// const aavi = () =>{
+
+// }
+
+// () => {
+
+// }
---
+++ scope.js
@@ -0,0 +1,33 @@
+// let a = 554;
+// var b = 45464;
+// const c = 445;
+// if(1==1){
+//     let a = 25;
+//     console.log(`inner loop is ${a}`)
+// }
+// console.log(`outside value is ${a}`)
+
+
+
+
+
+
+
+
+
+
+
+
+// -----------------------------------------------------------------------------------------------------
+function ravi(){
+    let a = "hey guys ";
+    console.log(a)
+    function nakul (){
+        let b = "tansen";
+        console.log(b)
+        console.log(a)
+    }
+    nakul()
+    
+}
+ravi()
