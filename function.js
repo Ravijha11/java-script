c923f5e90c666c83bdca9aab01ce6a0c6e0dca84
RAVINDRA JHA <ravijha110903@gmail.com>
2024-03-02 16:11:37 +0530

    arrow function

--- function.js
+++ function.js
@@ -1,20 +1,20 @@
-function twonumber(num1, num2){
-    let a = num1+num2;
-    return a;
+// function twonumber(num1, num2){
+//     let a = num1+num2;
+//     return a;
     
-}
- const a = twonumber(6,6);
+// }
+//  const a = twonumber(6,6);
 
-console.log(`the sum is ${a}`)
-function foo(i) {
-    if (i < 0) {
-      return;
-    }
-    console.log(`begin: ${i}`);
-    foo(i - 1);
-    console.log(`end: ${i}`);
-  }
-  foo(3);
+// console.log(`the sum is ${a}`)
+// function foo(i) {
+//     if (i < 0) {
+//       return;
+//     }
+//     console.log(`begin: ${i}`);
+//     foo(i - 1);
+//     console.log(`end: ${i}`);
+//   }
+//   foo(3);
   
   // Logs:
   // begin: 3
@@ -25,14 +25,78 @@ function foo(i) {
   // end: 1
   // end: 2
   // end: 3
-  function A(x) {
-    function B(y) {
-      function C(z) {
-        console.log(x + y + z);
-      }
-      C(3);
-    }
-    B(2);
-  }
-  A(1); // Logs 6 (which is 1 + 2 + 3)
+//   function A(x) {
+//     function B(y) {
+//       function C(z) {
+//         console.log(x + y + z);
+//       }
+//       C(3);
+//     }
+//     B(2);
+//   }
+//   A(1); // Logs 6 (which is 1 + 2 + 3)
+
+
+
+// const ravi = {
+//     name : "ravi jha",
+//     location : "Gurgaon",
+// }
+// function user(anyobject){
+//     console.log (`the name is ${anyobject.name} and location is ${anyobject.location}`)
+// }
+// user(ravi)
     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const a = {
+//     name : "Ravindra Jha",
+//     location :"Noida",
+//     enroll : "0901mc201051",
+//     fathername : "Taracahnadra Jha",
+//     mother : "suman"
+// }
+// // console.log(a.mother)
+// function ravi (anyobject){
+//     console.log(`name is ${anyobject.name } and location is ${anyobject.location } and enorll no. is ${anyobject.enroll}`);
+//     return;
+// }
+// ravi(a)
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let a = [5598,465,666,5955];
+function myarray(array){
+    return array[2];
+
+}
+console.log(myarray(a))
