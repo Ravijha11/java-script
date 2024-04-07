0ba6edfbd41a20a38f85a82ea652bdd4511e7091
RAVINDRA JHA <ravijha110903@gmail.com>
2024-04-07 10:10:59 +0530

    js

--- reduce.js
+++ reduce.js
@@ -1,3 +1,10 @@
 let  ravi = [1,2,,3,4,5,6,7,8,9,10];
-let nakul = ravi.filter((num) => num>6);
-console.log(nakul)
+// let nakul = ravi.filter((num) => num>6);
+// console.log(nakul)
+const matru = ravi.reduce((acc, curr) => {
+  console.log(`curr vvalue is  ${curr} and acc value is  ${acc}`);
+  return curr + acc
+
+
+})
+console.log(matru)
